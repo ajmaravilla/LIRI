776ccac05a3d50edf640b3466c205fba62097950
John Maravilla <john.maravilla@me.com>
2018-02-06 18:46:36 -0600

    LIRI initial commit

---
+++ keys.js
@@ -0,0 +1,18 @@
+//console.log('this is loaded');
+
+var twitterKeys = {
+	consumer_key: 'FVNTElwSR9ksh0ZxLliRvGuFL',
+	consumer_secret: 'Nz82esVa6lDV849XkbMmmR5U5azSGEXMnssxWq89ELoG7ittmV',
+	access_token_key: '5567312-eaLQEnFL4uat3wwnmkg27JNOKAxpihTEBhiLN1HBT0',
+	access_token_secret: 'DXhHFnktCoyp9MIHkHBjdb79coQa4da6IgalUyk7iqiRM',
+}
+
+var spotifyKeys = {
+	id: '8cb42fef7d0141859886c5095aaf463f',
+	secret: '99ac52767b5240919bc63ced5fe6987a'
+}
+
+module.exports = {
+	twitter: twitterKeys,
+	spotify: spotifyKeys
+}
